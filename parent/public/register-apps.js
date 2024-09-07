7c85ea0f703ef8063fcba02df17f3a6f03b230c5
jiaqh <2286592704@qq.com>
2024-09-07 14:40:20 +0800

    部署

--- parent/public/register-apps.js
+++ parent/public/register-apps.js
@@ -1,14 +1,22 @@
 document.subApps = [
+  // {
+  //   name: "childApp1",
+  //   entry: "//localhost:8081",
+  //   container: "#container-sub-app",
+  //   activeRule: "/child-app1",
+  // },
+  // {
+  //   name: "subAppReact",
+  //   entry: "//localhost:8000",
+  //   container: "#container-sub-app",
+  //   activeRule: "/sub-app-react",
+  // },
+
+  // 部署
   {
     name: "childApp1",
-    entry: "//localhost:8081",
+    entry: "http://192.168.18.171:8080",
     container: "#container-sub-app",
     activeRule: "/child-app1",
-  },
-  {
-    name: "subAppReact",
-    entry: "//localhost:8000",
-    container: "#container-sub-app",
-    activeRule: "/sub-app-react",
   },
 ];
